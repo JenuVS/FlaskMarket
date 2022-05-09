ec47c7b4224f1d058fde5cb73f71de1275047a21
jenu <jenu.s@edstem.com>
2022-05-09 17:03:30 +0530

    first commit

---
+++ market/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'eb17afbfdd345f6ab8f47d17'
+db = SQLAlchemy(app)
+
+from market import routes
