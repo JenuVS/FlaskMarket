832d520408da4257344d54a1f1f509f359d16401
jenu <jenu.s@edstem.com>
2022-05-10 12:43:06 +0530

    user authentification part1 completed

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'eb17afbfdd345f6ab8f47d17'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
