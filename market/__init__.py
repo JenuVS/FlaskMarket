b0f54cf5ad5b4943e658bf900067d11d4ca2d998
jenu <jenu.s@edstem.com>
2022-05-12 11:51:58 +0530

    authentification part2 completed

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'eb17afbfdd345f6ab8f47d17'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
